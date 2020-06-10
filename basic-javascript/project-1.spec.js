d9d5569bfaecc571a53802b05579f10d4127003f
rabah babaci <4rabah@gmail.com>
2020-06-09 21:05:38 -0700

    add a test suite for multiplyByTen

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,9 @@
-const helpers = require('./project-1');
+const helpers = require("./project-1");
 
-// start testing!
+describe("project one helpers", () => {
+  describe("multiplyByTen()", () => {
+    it("multiplies a number by 10", () => {
+      expect(helpers.multiplyByTen(3)).toEqual(30);
+    });
+  });
+});
